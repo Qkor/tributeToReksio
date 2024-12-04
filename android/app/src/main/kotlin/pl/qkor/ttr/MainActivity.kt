6f9bea9be350f4d9f3d64dabc06588c3082517f0
jk <jan.kukorowski@wp.pl>
2024-12-04 10:04:22 +0100

    initialize flutter project

---
+++ android/app/src/main/kotlin/pl/qkor/ttr/MainActivity.kt
@@ -0,0 +1,5 @@
+package pl.qkor.ttr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tribute to Reksio',
+      theme: ThemeData.dark(),
+      home: const GamePage(),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
