9e7d3e89430e3783e4d1dd4db1df855ec3440eea
jk <jan.kukorowski@wp.pl>
2024-12-04 12:07:23 +0100

    setting recognized spell

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:one_dollar_unistroke_recognizer/one_dollar_unistroke_recognizer.dart' as unistroke;
 import 'package:ttr/assets_manager.dart';
 import 'package:ttr/spell_painter.dart';
+import 'package:ttr/spells.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
@@ -29,6 +30,7 @@ class _GamePageState extends State<GamePage> {
 
   @override
   Widget build(BuildContext context) {
+    Spell? spell;
 
     if(!ready){
       return const Scaffold(body: Center(child: CircularProgressIndicator()));
@@ -45,13 +47,15 @@ class _GamePageState extends State<GamePage> {
         },
         onPanEnd: (_) {
           final recognized = unistroke.recognizeCustomUnistroke(_points);
-          if (recognized == null) {
-            print('No match found');
-          } else {
+          if (recognized != null && recognized.score>0.85) {
             print('Stroke recognized as ${recognized.name} with score ${recognized.score}');
+            spell = recognized.name;
+          } else{
+            spell = null;
           }
           setState(() {
-            _points.clear(); // Clear canvas after a spell attempt.
+            _points.clear();
+            print(spell);
           });
         },
         child: CustomPaint(
